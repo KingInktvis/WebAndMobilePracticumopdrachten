afb6a3273e777fb440ec0b446ceef76966dbd801
KingInktvis <rikdevries253@hotmail.nl>
2017-05-12 10:11:31 +0200

    Searchbar input changes in a better manner.

--- week2/omdb/src/index.js
+++ week2/omdb/src/index.js
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import SearchBar from './searchbar'
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = { term: 'ok'}
+    }
+
     render() {
         return (
           <div>
-              <SearchBar movie="batman" />
+              <SearchBar onInputChange={(term) => this.setState({term})} />
           </div>
         );
     }
--- week2/omdb/src/searchbar.js
+++ week2/omdb/src/searchbar.js
@@ -3,18 +3,19 @@ import React from 'react'
 class SearchBar extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {
-            value: ''
-        }
+        this.state = { term: ""}
     }
 
     render () {
         return (
-            <input value={this.props.movie}
-            onChange={(item) => {
-                this.props.movie = item.value;
-            }}/>
+            <input value={this.props.term}
+            onChange={(event) => {this.handleValueChange(event.target.value)}} />
         );
+    }
+
+    handleValueChange(newValue){
+        this.setState({term: newValue});
+        this.props.onInputChange(newValue);
     }
 }
 
